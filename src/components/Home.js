e82b841b1d0481e827a0342f547e1f20f437ecfb
errea <erreakezy@gmail.com>
2021-09-07 23:30:02 +0100

    Modify Home.js file

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,9 +6,30 @@ import {
 import { useSelector } from 'react-redux';
 import Header from './Header';
 
-
 const Home = () => {
+  const { url } = useRouteMatch();
+  // const match = useRouteMatch();
 
+  const countries = useSelector((state) => state.covidReducer.covidCountries);
+  // console.log(countries);
+  const countriesList = countries.map((country) => (
+    <div key={country.name[0]} className="country-card">
+      <Link href="/#" to={`${url}${country.name[0].toLowerCase()}`}>
+        <div key={country.name[0]} id={country.name[0]} className="country-link">
+          <p>{country.name[0]}</p>
+          <p>{country.confirmed}</p>
+          <p>{country.deaths}</p>
+        </div>
+      </Link>
+    </div>
+  ));
+
+  return (
+    <>
+      <Header title="Countries" backButton="App" />
+      { countriesList }
+    </>
+  );
 };
 
 export default Home;
