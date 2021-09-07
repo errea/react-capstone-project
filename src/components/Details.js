304222e72b3bd503e83ef0b89c7b74a4e7833d68
errea <erreakezy@gmail.com>
2021-09-07 23:24:38 +0100

    Fix eslint error with wild character

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import {
+  useRouteMatch,
+} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Header from './Header';
+import getDetails from '../redux/slices/detailsSlice.js';
 
 const Details = ({ name }) => (
   <>
     <Header title="Contry details" backButton="Countries" />
     <h2>Details Page</h2>
     <p>{name}</p>
-    <p>Today's Death: </p>
+    <p>Today Death: </p>
     <p>{}</p>
   </>
 );
