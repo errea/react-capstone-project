790e01dd0ffa93bc49a8fb36c6543993fa19b02a
errea <erreakezy@gmail.com>
2021-09-07 12:06:24 +0100

    Add Home.js file to components dir

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Link,
+  useRouteMatch,
+} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Header from './Header';
+
+
+const Home = () => {
+
+};
+
+export default Home;
