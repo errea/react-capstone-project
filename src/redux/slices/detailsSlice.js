79d17a3c041cb9136e941c15e3dec33ffc5e8011
errea <erreakezy@gmail.com>
2021-09-08 00:26:34 +0100

    Add detailsURL + fetch API with response +

---
+++ src/redux/slices/detailsSlice.js
@@ -0,0 +1,23 @@
+import dayjs from 'dayjs';
+
+const GET_COVID_DETAILS = 'covid_details/detailsslice/GET_COVID_DETAILS';
+const GET_COVID_SUCCESS_DETAILS = 'covid_details/detailsslice/GET_COVID_SUCCESS_DETAILS';
+const GET_COVID_ERR_DETAILS = 'covid_details/detailsslice/GET_COVID_ERR_DETAILS';
+
+const today = dayjs().format('YYYY-MM-DD');
+
+const detailsURL = `https://api.covid19tracking.narrativa.com/api/${today}/country`;
+
+const getDetails = (country) => async (dispatch) => {
+  dispatch({ type: GET_COVID_DETAILS });
+  const response = await fetch(detailsURL);
+  const details = await response.json();
+  return dispatch({ type: GET_COVID_SUCCESS_DETAILS, details });
+};
+
+export {
+  getDetails as default,
+  GET_COVID_DETAILS,
+  GET_COVID_SUCCESS_DETAILS,
+  GET_COVID_ERR_DETAILS,
+};
