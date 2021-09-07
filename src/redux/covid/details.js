be153800277f88ff70c188643d6dd2e626619c40
errea <erreakezy@gmail.com>
2021-09-08 00:28:45 +0100

    Add details.js with reducer functions

--- src/redux/covid/details.js
+++ src/redux/covid/details.js
@@ -29,7 +29,7 @@ const reducer = (state = initialState, action) => {
       return { 
           ...state, pending: false, error: action.error };
           default:
-          return state;
+        return state;
   }
 };
 
