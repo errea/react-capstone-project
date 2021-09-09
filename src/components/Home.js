569ab7b6cc983f4ee9d0a0aabe58db372bcd1371
errea <erreakezy@gmail.com>
2021-09-09 11:52:07 +0100

    Modify Home.js + add filter method to filter by range

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,75 @@
-import React from 'react';
-import {
-  Link,
-  useRouteMatch,
-} from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Header from './Header';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getData } from '../redux/covidhomepage/covidhomepage';
+import { filterCountries } from '../redux/filterpage/filterpage';
+import './Home.css';
+import HomeCountryCard from './HomeCountryCard';
 
-const Home = () => {
-  const { url } = useRouteMatch();
-  // const match = useRouteMatch();
+const Homepage = () => {
+  const dispatch = useDispatch();
 
-  const countries = useSelector((state) => state.covidReducer.covidCountries);
-  // console.log(countries);
-  const countriesList = countries.map((country) => (
-    <div key={country.name[0]} className="country-card">
-      <Link href="/#" to={`${url}${country.name[0].toLowerCase()}`}>
-        <div key={country.name[0]} id={country.name[0]} className="country-link">
-          <p>{country.name[0]}</p>
-          <p>{country.confirmed}</p>
-          <p>{country.deaths}</p>
-        </div>
-      </Link>
-    </div>
-  ));
+  const data = useSelector((state) => state.covidData);
+  const countries = useSelector((state) => state.countries);
+
+  useEffect(() => {
+    if (data.length === 0) {
+      dispatch(getData());
+    }
+  }, []);
+
+  const handleSelection = (e) => {
+    dispatch(filterCountries({ value: e.target.value, data }));
+  };
+
+  const dataToUse = countries.filtered.length === 0 ? data : countries.filtered;
+  const randomNum = Math.round(Math.random() * dataToUse.length);
 
   return (
-    <>
-      <Header title="Countries" backButton="App" />
-      { countriesList }
-    </>
+    <main>
+      <ul className="home-list">
+        {
+          dataToUse.length === 0
+            ? null
+            : (
+              <li className="home-main-item">
+                <div className="main-item-image" />
+                <div className="main-item-name">
+                  <Link to={{ pathname: '/details', search: `country=${dataToUse[randomNum][0]}` }}>{dataToUse[randomNum][0]}</Link>
+                  <p className="main-item-info">{`${dataToUse[randomNum][1].today_new_confirmed} new cases`}</p>
+                </div>
+              </li>
+            )
+        }
+        {
+          dataToUse.length === 0
+            ? null
+            : (
+              <div className="home-top-divider">
+                <p>STATS BY COUNTRY</p>
+                <label htmlFor="filter-select">
+                  Filter
+                  <select value={countries.value} onChange={(e) => handleSelection(e)} id="filter-select">
+                    <option value="all">All</option>
+                    <option value="0">0 - 100</option>
+                    <option value="101">101 - 500</option>
+                    <option value="501">501 - 1000</option>
+                    <option value="1000">1000+</option>
+                  </select>
+                </label>
+              </div>
+            )
+        }
+        {dataToUse.map((d) => (
+          <HomeCountryCard
+            key={d[0]}
+            country={d[0]}
+            newConfirmed={d[1].today_new_confirmed}
+          />
+        ))}
+      </ul>
+    </main>
   );
 };
 
-export default Home;
+export default Homepage;
