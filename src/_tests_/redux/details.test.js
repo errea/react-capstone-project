9801e32e2fb31b3d32d0d3ed59933da61f46c8d0
errea <erreakezy@gmail.com>
2021-09-09 15:41:30 +0100

    Fix linters error

--- src/_tests_/redux/details.test.js
+++ src/_tests_/redux/details.test.js
@@ -1,4 +1,4 @@
-import filterReducer, { filterData } from '../../redux/detailspage/details';
+import filterReducer, { filterData } from '../../redux/details/detailspage';
 
 const initialState = {};
 
